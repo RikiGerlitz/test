a4e7f70f1fdbd174eaa2f55cb56e2fc1dcadfa8c
RikiGerlitz <rikig4114@gmail.com>
2023-11-29 13:48:02 +0200

    change secret

--- models/userModel.js
+++ models/userModel.js
@@ -2,7 +2,7 @@
 const mongoose = require("mongoose");
 const Joi = require("joi");
 const jwt = require("jsonwebtoken");
-const { config } = require("dotenv");
+const { config } = require("../config/secret");
 const userSchema = new mongoose.Schema({
   name:String,
   email:String,
